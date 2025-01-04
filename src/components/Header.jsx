6ff461cf2b12cc9bea9f5c8ccbf5ed5403091f6d
SaberLS <patryk.platek19@gmail.com>
2025-01-04 16:47:41 +0100

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logoipsum-346.svg";
+import { FiAlignJustify } from "react-icons/fi";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="nav__logo">
+          <img src={Logo} alt="Navbar Logo" />
+        </Link>
+        <ul className="nav__menu">
+          <li>
+            <Link to="/profile">profile</Link>
+          </li>
+          <li>
+            <Link to="/create">create</Link>
+          </li>
+          <li>
+            <Link to="/authors">authors</Link>
+          </li>
+          <li>
+            <Link to="/logout">logout</Link>
+          </li>
+        </ul>
+        <button className="nav__toggle-btn">
+          <FiAlignJustify />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
