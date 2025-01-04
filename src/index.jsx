d142ae3dc3d4a85e1b8e917352762554e31bd727
SaberLS <patryk.platek19@gmail.com>
2025-01-04 14:32:11 +0100

    routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Register from "./pages/Register";
+import UserProfile from "./pages/UserProfile";
+import LoginPage from "./pages/LoginPage";
+import Autorhors from "./pages/Authors";
+import CreatePost from "./pages/CreatePost";
+import CategoryPosts from "./pages/CategoryPosts";
+import AuthorPosts from "./pages/AuthorPosts";
+import Logout from "./pages/Logout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts/:id", element: <PostDetail /> },
+      { path: "register", element: <Register /> },
+      { path: "profile/:id", element: <UserProfile /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "authors", element: <Autorhors /> },
+      { path: "create", element: <CreatePost /> },
+      { path: "posts/categories/:category", element: <CategoryPosts /> },
+      { path: "posts/users/:id", element: <AuthorPosts /> },
+      { path: "mympsts/:id", element: <AuthorPosts /> },
+      { path: "posts/:id/edit", element: <AuthorPosts /> },
+      { path: "logout", element: <Logout /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-reportWebVitals();
