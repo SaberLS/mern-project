0055a548e0b5b7e15efc0a91999b575450b89a52
SaberLS <patryk.platek19@gmail.com>
2025-01-06 16:43:34 +0100

    Mobile NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logoipsum-346.svg";
 import { FiAlignJustify } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
+  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800);
+
+  const closeNavHandler = () => setIsNavShowing(!isNavShowing);
   return (
     <nav>
       <div className="container nav__container">
         <Link to="/" className="nav__logo">
           <img src={Logo} alt="Navbar Logo" />
         </Link>
-        <ul className="nav__menu">
-          <li>
-            <Link to="/profile/something">profile</Link>
-          </li>
-          <li>
-            <Link to="/create">create</Link>
-          </li>
-          <li>
-            <Link to="/authors">authors</Link>
-          </li>
-          <li>
-            <Link to="/logout">logout</Link>
-          </li>
-        </ul>
-        <button className="nav__toggle-btn">
-          <FiAlignJustify />
+        {isNavShowing && (
+          <ul className="nav__menu">
+            <li onClick={closeNavHandler}>
+              <Link to="/profile/something">profile</Link>
+            </li>
+            <li onClick={closeNavHandler}>
+              <Link to="/create">create</Link>
+            </li>
+            <li onClick={closeNavHandler}>
+              <Link to="/authors">authors</Link>
+            </li>
+            <li onClick={closeNavHandler}>
+              <Link to="/logout">logout</Link>
+            </li>
+          </ul>
+        )}
+        <button
+          className="nav__toggle-btn"
+          onClick={() => setIsNavShowing(!isNavShowing)}
+        >
+          {isNavShowing ? <IoMdClose /> : <FiAlignJustify />}
         </button>
       </div>
     </nav>
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "indent",
+  "link",
+  "image",
+];
+const POST_CATEGORIES = [
+  "Agriculture",
+  "Business",
+  "Education",
+  "Entertainment",
+  "Art",
+  "Investment",
+  "Uncategorized",
+  "Weather",
+];
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Uncategorized");
+  const [description, setDescription] = useState("");
+  const [thumbnail, setThumbnail] = useState("Thumbnail");
+
+  return (
+    <section className="create-post">
+      <div className="container">
+        <h2>Create Post</h2>
+        <p className="form__eror-message">This is an error message</p>
+        <form className="form create-post__form">
+          <input
+            type="text"
+            placeholder="Title"
+            name=""
+            id=""
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <select
+            name="category"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {POST_CATEGORIES.length ? (
+              POST_CATEGORIES.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))
+            ) : (
+              <option value="no categories">no categories</option>
+            )}
+          </select>
+          <ReactQuill
+            modules={modules}
+            formats={formats}
+            value={description}
+            onChange={setDescription}
+          />
+          <input
+            type="file"
+            name="thumbnail"
+            id="thumbnail"
+            onChange={(e) => setThumbnail(e.target.files[0])}
+            accept="png, jpg, jpeg"
+          />
+          <button type="submit" className="btn primary">
+            Create
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default CreatePost;
